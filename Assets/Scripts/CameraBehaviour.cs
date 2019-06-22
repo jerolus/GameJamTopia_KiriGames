833ce42f641baf94a0699c75da9a7e562097e20b
Jesús María Parral Puebla <jesusmaria31@gmail.com>
2019-06-22 13:43:51 +0200

    Sun animation added. Prefab edited. Scene update. Camera behaviour improved.

---
+++ Assets/Scripts/CameraBehaviour.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBehaviour : MonoBehaviour
+{
+    public Transform player;
+    public float offset;
+
+    private void Update()
+    {
+        transform.position = new Vector3(transform.position.x, transform.position.y, player.position.z + offset);
+    }
+}
